c1a188e03923c8cadc1d4098ed340884015b2289
wiliami <wiliaup97@gmail.com>
2024-07-19 12:36:44 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+import Hapi from '@hapi/hapi'
+
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 3334,
+        host: 'localhost'
+    });
+
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World';
+        }
+    })
+
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+
+
+console.log(init());
